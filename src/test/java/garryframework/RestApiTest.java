6b72135c48357b06da9cd882bd1245eed5f886b2
Gaurav <forwardtolalit@gmail.com>
2020-02-18 15:11:37 +0530

    code commied by american guy

--- src/test/java/garryframework/RestApiTest.java
+++ src/test/java/garryframework/RestApiTest.java
@@ -10,6 +10,11 @@ public class RestApiTest {
 		System.out.println("postJira");
 		System.out.println("postjira2");
 		System.out.println("postJira3");
+		System.out.println("postJira4");
+		System.out.println("postJira5");
+		System.out.println("postJira6");
+		System.out.println("postJira7");
+		
 	}
 	
 	@Test
